341414f887cbc6670ee7895bdec6e2d98995da92
Danilo Silva <danilo.silva@gmail.com>
2017-08-08 21:31:36 -0300

    Exercicio aula 2

--- Request.php
+++ Request.php
@@ -17,81 +17,61 @@ class Request
         $this->query_string = $query_string;
         }
 
-    /**
-     * @return mixed
-     */
+   
     public function getMethod()
     {
         return $this->method;
     }
 
-    /**
-     * @param mixed $method
-     */
+    
     public function setMethod($method)
     {
         $this->method = $method;
     }
 
-    /**
-     * @return mixed
-     */
+    
     public function getProtocol()
     {
         return $this->protocol;
     }
 
-    /**
-     * @param mixed $protocol
-     */
+    
     public function setProtocol($protocol)
     {
         $this->protocol = $protocol;
     }
 
-    /**
-     * @return mixed
-     */
+    
     public function getHost()
     {
         return $this->host;
     }
 
-    /**
-     * @param mixed $host
-     */
+    
     public function setHost($host)
     {
         $this->host = $host;
     }
 
-    /**
-     * @return mixed
-     */
+    
     public function getPath()
     {
         return $this->path;
     }
 
-    /**
-     * @param mixed $path
-     */
+    
     public function setPath($path)
     {
         $this->path = $path;
     }
 
-    /**
-     * @return mixed
-     */
+    
     public function getQueryString()
     {
         return $this->query_string;
     }
 
-    /**
-     * @param mixed $query_string
-     */
+    
     public function setQueryString($query_string)
     {
         $this->query_string = $query_string;
